56aea10575d51f1a6655f597752a17c98365c949
Lennert Verbeeck <lennert.verbeeck@gmail.com>
2023-06-06 15:08:24 +0200

    baseline finally done

--- veggideas/model.py
+++ veggideas/model.py
@@ -4,18 +4,3 @@ from veggideas.load_data import load_val_data
 
 train_data = load_train_data()
 val_data = load_val_data()
-
-def split_data(data):
-    X = []
-    y = []
-
-
-    for images, labels in data:
-        X.append(images)
-        y.append(labels)
-    X = tf.concat(X, axis=0)
-    y = tf.concat(y, axis=0)
-
-    return X, y
-
-X_train, y_train = split_data(train_data)
