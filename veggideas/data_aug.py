9c92524ba162f5bbaa0af594e24e30287231d5e4
Liesel Vranckx <liesel.vranckx@gmail.com>
2023-06-06 16:42:21 +0200

    changed data_aug

--- veggideas/data_aug.py
+++ veggideas/data_aug.py
@@ -1,84 +1,13 @@
-from raw_data import train_data
-from raw_data import val_data
-from raw_data import test_data
-from veggideas.load_data import load_train_data
 import tensorflow as tf
 from keras import layers
-import matplotlib as plt
 
-"""
-get_label_name = load_train_data.features['label'].int2str
+def preprocessing_image():
+    resize_and_rescale = tf.keras.Sequential([
+        layers.Rescaling(1./255)
+    ])
 
-image, label = next(iter(train_data))
-_ = plt.imshow(image)
-_ = plt.title(get_label_name(label))
-"""
-image = load_train_data()
-
-IMG_SIZE = 180
-resize_and_rescale = tf.keras.Sequential([
-  layers.Resizing(IMG_SIZE, IMG_SIZE),
-  layers.Rescaling(1./255)
-])
-
-result = resize_and_rescale(image)
-
-data_augmentation = tf.keras.Sequential([
-  layers.RandomFlip("horizontal_and_vertical"),
-  layers.RandomRotation(0.2),
-])
-
-# Add the image to a batch.
-image = tf.cast(tf.expand_dims(image, 0), tf.float32)
-
-
-plt.figure(figsize=(10, 10))
-for i in range(9):
-  augmented_image = data_augmentation(image)
-  ax = plt.subplot(3, 3, i + 1)
-  plt.imshow(augmented_image[0])
-  plt.axis("off")
-
-
-model = tf.keras.Sequential([
-  # Add the preprocessing layers you created earlier.
-  resize_and_rescale,
-  data_augmentation,
-  layers.Conv2D(16, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  # Rest of your model.
-])
-
-
-aug_ds = train_data.map(
-  lambda x, y: (resize_and_rescale(x, training=True), y))
-
-
-
-batch_size = 32
-AUTOTUNE = tf.data.AUTOTUNE
-
-def prepare(ds, shuffle=False, augment=False):
-  # Resize and rescale all datasets.
-  ds = ds.map(lambda x, y: (resize_and_rescale(x), y),
-              num_parallel_calls=AUTOTUNE)
-
-  if shuffle:
-    ds = ds.shuffle(1000)
-
-  # Batch all datasets.
-  ds = ds.batch(batch_size)
-
-  # Use data augmentation only on the training set.
-  if augment:
-    ds = ds.map(lambda x, y: (data_augmentation(x, training=True), y),
-                num_parallel_calls=AUTOTUNE)
-
-  # Use buffered prefetching on all datasets.
-  return ds.prefetch(buffer_size=AUTOTUNE)
-
-
-
-train_ds = prepare(train_data, shuffle=True, augment=True)
-val_ds = prepare(val_data)
-test_ds = prepare(test_data)
+    data_augmentation = tf.keras.Sequential([
+        layers.RandomFlip("horizontal_and_vertical"),
+        layers.RandomRotation(0.2),
+        ])
+    return resize_and_rescale, data_augmentation
