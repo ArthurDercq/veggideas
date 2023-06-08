94aaa746d2725b2cf9382f7ab3e9f2186aede845
Liesel Vranckx <liesel.vranckx@gmail.com>
2023-06-08 12:20:44 +0200

    bounding box2

--- veggideas/bounding_box.py
+++ veggideas/bounding_box.py
@@ -7,63 +7,92 @@ from keras import models
 from keras import optimizers
 from veggideas.load_data import load_train_data, load_val_data
 
+
+
 train_data = load_train_data()
 val_data = load_val_data()
-model = load_non_trainable_model()
+base_model = load_non_trainable_model()
 
-# Define the path to save the TFRecord file
-tfrecord_file = 'augmented_dataset.tfrecord'
 
-# Create an instance of ImageDataGenerator for data augmentation
-datagen = ImageDataGenerator(
-    rotation_range=20,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True,
-    fill_mode='nearest'
-)
+def data_augmentation():
+    datagen = ImageDataGenerator(
+        rotation_range=20,  # Rotate images by 20 degrees
+        width_shift_range=0.2,  # Shift images horizontally by 20% of the total width
+        height_shift_range=0.2,  # Shift images vertically by 20% of the total height
+        shear_range=0.2,  # Apply shear transformation with a shear intensity of 20%
+        zoom_range=0.2,  # Apply zoom transformation with a zoom range of 20%
+        horizontal_flip=True,  # Flip images horizontally
+        fill_mode='nearest'  # Fill newly created pixels during transformations with the nearest value
+    )
 
-# Create an empty list to store augmented images and labels
-augmented_images = []
-augmented_labels = []
+    # Create an empty list to store augmented images and labels
+    augmented_images = []
+    augmented_labels = []
 
-# Iterate over each batch in the training dataset
-for images, labels in train_data:
-    # Apply data augmentation to the batch
-    augmented_batch = datagen.flow(images, labels, batch_size=images.shape[0])
-    augmented_images.extend(augmented_batch[0][0])
-    augmented_labels.extend(augmented_batch[0][1])
+    # Iterate over each batch in the training dataset
+    for images, labels in train_data:
+        # Apply data augmentation to the batch
+        augmented_batch = datagen.flow(images, labels, batch_size=images.shape[0])
+        augmented_images.extend(augmented_batch[0][0])
+        augmented_labels.extend(augmented_batch[0][1])
 
-# Convert the augmented images and labels to TensorFlow tensors
-augmented_images = tf.convert_to_tensor(augmented_images)
-augmented_labels = tf.convert_to_tensor(augmented_labels)
+    # Convert the augmented images and labels to TensorFlow tensors
+    augmented_images = tf.convert_to_tensor(augmented_images)
+    augmented_labels = tf.convert_to_tensor(augmented_labels)
 
-# Create a new dataset using the augmented images and labels
-augmented_train_data = tf.data.Dataset.from_tensor_slices((augmented_images, augmented_labels))
+    # Create a new dataset using the augmented images and labels
+    augmented_train_data = tf.data.Dataset.from_tensor_slices((augmented_images, augmented_labels))
 
-# Create a TFRecord writer to save the augmented dataset
-writer = tf.data.experimental.TFRecordWriter(tfrecord_file)
+    return augmented_train_data
 
-# Iterate over each example in the augmented dataset and write it to the TFRecord file
-for image, label in augmented_train_data:
-    # Serialize the image and label tensors
-    image_bytes = tf.io.serialize_tensor(image)
-    label_bytes = tf.io.serialize_tensor(label)
 
-    # Create a feature dictionary
-    feature_dict = {
-        'image': tf.train.Feature(bytes_list=tf.train.BytesList(value=[image_bytes.numpy()])),
-        'label': tf.train.Feature(bytes_list=tf.train.BytesList(value=[label_bytes.numpy()]))
-    }
 
-    # Create an Example proto and serialize it to a string
-    example = tf.train.Example(features=tf.train.Features(feature=feature_dict))
-    example_string = example.SerializeToString()
+""""
+BOUNDING BOX
 
-    # Write the serialized Example to the TFRecord file
-    writer.write(example_string)
+"""
 
-# Close the TFRecord writer
-writer.close()
+
+def building_the_model():
+    def add_last_layers(base_model):
+        '''Take a pre-trained model, set its parameters as non-trainable, and add additional trainable layers on top'''
+        resize_and_rescale = tf.keras.Sequential([
+            layers.Rescaling(1./255)])
+
+        base_model = base_model
+        flattening_layer = layers.Flatten()
+        dense_layer = layers.Dense(128, activation=tf.keras.layers.LeakyReLU(alpha=0.1))
+        dense_layer_2 = layers.Dense(64, activation='relu')
+        reg_l2 = regularizers.L2(0.001)
+        dense_layer_reg = layers.Dense(128, activation='relu', bias_regularizer=reg_l2)
+        conv2D_256 = layers.Conv2D(512, 3, padding='same', activation='relu')
+        conv2D_512 = layers.Conv2D(512, 3, padding='same', activation='relu')
+        maxpool_layer = layers.MaxPool2D(pool_size=(2,2))
+        prediction_layer = layers.Dense(15, activation='softmax')
+
+        model = models.Sequential([
+            resize_and_rescale,
+            base_model,
+            conv2D_256,
+            maxpool_layer,
+            conv2D_512,
+            maxpool_layer,
+            flattening_layer,
+            dense_layer,
+            dense_layer_reg,
+            dense_layer_2,
+            prediction_layer
+            ])
+
+
+        opt = optimizers.Adam(learning_rate=1e-5)
+        model.compile(loss='categorical_crossentropy',
+                    optimizer=opt,
+                    metrics=['accuracy'])
+        return model
+
+    augmented_train_data = data_augmentation()
+    model_transfer = add_last_layers(base_model)
+    model_transfer.fit(augmented_train_data, batch_size=32, epochs=10, validation_data=val_data)
+
+    return model_transfer
