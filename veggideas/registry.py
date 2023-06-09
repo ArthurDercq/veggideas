9d12d5ea76311aaf4d0786edfb788dbb8eb88617
Arthur Dercq <arthur.dercq@skynet.be>
2023-06-09 13:59:10 +0200

    made the registry work

--- veggideas/registry.py
+++ veggideas/registry.py
@@ -5,7 +5,7 @@ import os
 import pickle
 from google.cloud import storage
 from veggideas.params import *
-from colorama import Fore, Style
+#from colorama import Fore, Style
 
 
 
@@ -51,7 +51,7 @@ def load_model() -> keras.Model:
     """
 
     if MODEL_TARGET == "local":
-        print(Fore.BLUE + f"\nLoad latest model from local registry..." + Style.RESET_ALL)
+        print("\nLoad latest model from local registry...")
 
         # Get the latest model version name by the timestamp on disk
         local_model_directory = os.path.join(LOCAL_REGISTRY_PATH, "models")
@@ -62,7 +62,7 @@ def load_model() -> keras.Model:
 
         most_recent_model_path_on_disk = sorted(local_model_paths)[-1]
 
-        print(Fore.BLUE + f"\nLoad latest model from disk..." + Style.RESET_ALL)
+        print("\nLoad latest model from disk...")
 
         latest_model = keras.models.load_model(most_recent_model_path_on_disk)
 
@@ -71,7 +71,7 @@ def load_model() -> keras.Model:
         return latest_model
 
     elif MODEL_TARGET == "gcs":
-        print(Fore.BLUE + f"\nLoad latest model from GCS..." + Style.RESET_ALL)
+        print("\nLoad latest model from GCS...")
 
         client = storage.Client()
         blobs = list(client.get_bucket(BUCKET_NAME).list_blobs(prefix="model"))
