a1a003d24c02b0b81ca8f2a17360cc0de5aa3d1f
Lennert Verbeeck <lennert.verbeeck@gmail.com>
2023-06-12 16:17:49 +0200

    finally made the deploy work

--- veggideas/registry.py
+++ veggideas/registry.py
@@ -72,24 +72,18 @@ def load_model() -> keras.Model:
 
     elif MODEL_TARGET == "gcs":
         print("\nLoad latest model from GCS...")
-
         client = storage.Client()
         blobs = list(client.get_bucket(BUCKET_NAME).list_blobs(prefix="model"))
 
-        try:
-            latest_blob = max(blobs, key=lambda x: x.updated)
-            latest_model_path_to_save = os.path.join(LOCAL_REGISTRY_PATH, latest_blob.name)
-            latest_blob.download_to_filename(latest_model_path_to_save)
 
-            latest_model = keras.models.load_model(latest_model_path_to_save)
+        latest_blob = max(blobs, key=lambda x: x.updated)
+        latest_blob.download_to_filename("model.h5")
 
-            print("✅ Latest model downloaded from cloud storage")
+        latest_model = keras.models.load_model("model.h5")
 
-            return latest_model
-        except:
-            print(f"\n❌ No model found in GCS bucket {BUCKET_NAME}")
+        print("✅ Latest model downloaded from cloud storage")
 
-            return None
+        return latest_model
 
 
 def save_results(params: dict, metrics: dict) -> None:
